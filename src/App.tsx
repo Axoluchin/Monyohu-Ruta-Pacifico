e5721dbb57b59974fc6e081b223c17655d13facc
Damian Huchin <huchin.h.damian@gmail.com>
2023-04-07 21:13:50 -0600

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className="text-emerald-400 font-bold">
+        Hola mundo
+      </h1>
+    </div>
+  )
+}
+
+export default App
