1c3faea98b0583142827f44155e1e3dd7e27380b
Damian Huchin <huchin.h.damian@gmail.com>
2023-04-08 18:06:11 -0600

    More sedes

--- src/pages/Sedes.tsx
+++ src/pages/Sedes.tsx
@@ -1,4 +1,5 @@
 import CardSede from '../components/CardSede'
+import { Card } from 'flowbite-react'
 
 const sedesList = [
   {
@@ -27,25 +28,50 @@ const sedesList = [
     telefonos: ['555 312 6501', '555 312 7031'],
   },
   {
-    name: 'Colón',
+    name: 'Toluca',
+    telefonos: ['722 217 1276'],
+  },
+  {
+    name: 'Toluca - sucursal en Colón',
     telefonos: ['722 320 2024'],
+  },
+  {
+    name: 'Lerma - Ocoyoacac',
+    telefonos: ['728 103 3738', '722 688 6577'],
+  },
+  {
+    name: 'Morelia Norte',
+    telefonos: ['443 316 6924'],
+  },
+  {
+    name: 'Morelia Sur',
+    telefonos: ['443 234 0110'],
   },
 ]
 
 const Sedes = () => {
   return (
     <div className="md:grid md:grid-cols-2 gap-10 static">
-      <div className='px-2 md:pl-16 pt-8'>
-        <h1 className="text-center text-5xl font-bold text-mark">Sedes</h1>
-        <p className="text-center text-xl mt-8">Nuestro trabajo nos respalda</p>
+      <div className="px-2 md:pl-16 pt-8">
+        <Card>
+          <h1 className="text-center text-5xl font-bold text-mark">Sedes</h1>
+          <p className="text-center text-xl mt-8">
+            Nuestro trabajo nos respalda
+          </p>
+        </Card>
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 my-12">
           {sedesList.map((sede) => (
             <CardSede {...sede} key={sede.name} />
           ))}
         </div>
       </div>
-      <div className='bg-landing order-1 md:order-2'>
-        <img src="/images/Phone.png" alt="Phone man" style={{height: '80vh'}} className='self-center md:fixed right-1/5 md:my-16 object-contain'/>
+      <div className="bg-landing order-1 md:order-2">
+        <img
+          src="/images/Phone.png"
+          alt="Phone man"
+          style={{ height: '80vh' }}
+          className="self-center md:fixed right-1/5 md:my-16 object-contain"
+        />
       </div>
     </div>
   )
