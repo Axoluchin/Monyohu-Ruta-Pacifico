12c9333f71cea3d8d937dfee57dc18517e037911
Damian Huchin <huchin.h.damian@gmail.com>
2023-04-10 14:02:52 -0600

    fix carousel

--- src/components/MyCarousel.tsx
+++ src/components/MyCarousel.tsx
@@ -1,28 +1,29 @@
 import { Carousel } from 'flowbite-react'
 
 const MyCarousel = () => (
-  <div className="grid gap-4 m-auto aspect-video shadow-xl">
-    <Carousel className="" slide={false}>
-      <video controls muted autoPlay>
-        <source src="/video/VideoTestimonios.mp4" type="video/mp4" />
-      </video>
-      <img
-        src="/images/NuevaSede.png"
-        alt="..."
-        className="h-full object-contain"
-      />
-      <img
-        src="/images/Portada1.png"
-        alt="..."
-        className="h-full object-contain"
-      />
-      <img
-        src="/images/Portada2.png"
-        alt="..."
-        className="h-full object-contain"
-      />
-    </Carousel>
-  </div>
+  <Carousel
+    className="h-full grid gap-4 m-auto aspect-video shadow-xl"
+    slide={false}
+  >
+    <video controls muted autoPlay>
+      <source src="/video/VideoTestimonios.mp4" type="video/mp4" />
+    </video>
+    <img
+      src="/images/NuevaSede.png"
+      alt="..."
+      className="h-full object-contain"
+    />
+    <img
+      src="/images/Portada1.png"
+      alt="..."
+      className="h-full object-contain"
+    />
+    <img
+      src="/images/Portada2.png"
+      alt="..."
+      className="h-full object-contain"
+    />
+  </Carousel>
 )
 
 export default MyCarousel
