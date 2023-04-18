7ba4a8fd9d40f92026d8cbb9d6e68e5b0e602ac7
Damián Hernández Huchín <60200578+Axoluchin@users.noreply.github.com>
2023-04-18 14:51:17 -0600

    Change landing message

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => (
     <img src="/images/intro.png" className="hidden md:block md:self-center h-full object-cover" />
     <div className="self-center p-8">
       <h1 className="text-5xl mb-8 font-bold text-mark">Grupo Monyohu</h1>
-      <p>Llámanos o manda WA al contacto de la matriz <a href="tel:5585265946" className="text-mark font-bold underline">55 8526 5946</a></p>
+      <p>Llámanos o manda WA al contacto de las oficinas centrales <a href="tel:5585265946" className="text-mark font-bold underline">55 8526 5946</a></p>
       <ul className="grid md:grid-cols-2 text-lg list-disc ml-10 gap-3 mt-8 mb-4">
         <li className="md:col-span-2">
           Siempre otorgamos el{' '}
