c98f880bd69b4113d97f4b65c8878dbb317c6b69
Damián Hernández Huchín <huchin.h.damian@gmail.com>
2023-04-22 15:30:55 -0600

    Order sedes

--- src/pages/Sedes.tsx
+++ src/pages/Sedes.tsx
@@ -1,100 +1,100 @@
-import { useEffect, useState, useCallback } from 'react'
-import MagnifyingGlassIcon from '@heroicons/react/24/outline/MagnifyingGlassIcon'
-import { Card, TextInput } from 'flowbite-react'
+import { useEffect, useState, useCallback } from "react";
+import MagnifyingGlassIcon from "@heroicons/react/24/outline/MagnifyingGlassIcon";
+import { Card, TextInput } from "flowbite-react";
 
-import CardSede from '../components/CardSede'
+import CardSede from "../components/CardSede";
 
 interface sede {
-  name: string
-  telefonos: string[]
+  name: string;
+  telefonos: string[];
 }
 
 const sedesList: sede[] = [
   {
-    name: 'Irapuato | Salamanca',
-    telefonos: ['462 660 1289', '462 635 6264'],
+    name: "Acapulco",
+    telefonos: ["744 482 1780", "744 482 1852"],
   },
   {
-    name: 'Culiacán',
-    telefonos: ['667 749 6931', '667 172 1162'],
+    name: "Aguascalientes",
+    telefonos: ["449 994 1503", "449 994 2007"],
   },
   {
-    name: 'Celaya',
-    telefonos: ['461 617 5004', '461 103 1098'],
+    name: "Cancún",
+    telefonos: ["998 138 8210", "998 138 8212"],
   },
   {
-    name: 'San Luis Potosí',
-    telefonos: ['444 688 8097', '444 688 8098'],
+    name: "Celaya",
+    telefonos: ["461 617 5004", "461 103 1098"],
   },
   {
-    name: 'CDMX | Tlalpan | Naucalpan',
-    telefonos: ['555 312 6501', '555 312 7031'],
+    name: "CDMX | Tlalpan | Naucalpan",
+    telefonos: ["555 312 6501", "555 312 7031"],
   },
   {
-    name: 'Toluca | Atlacomulco | Zinacantepec',
-    telefonos: ['722 270 2114', '722 270 2115'],
+    name: "Culiacán",
+    telefonos: ["667 749 6931", "667 172 1162"],
   },
   {
-    name: 'Toluca sucursal Colón | Valle de Bravo | Cacalomacán | Capultitán',
-    telefonos: ['722 277 8960', '722 2171276'],
+    name: "Irapuato | Salamanca",
+    telefonos: ["462 660 1289", "462 635 6264"],
   },
   {
-    name: 'Lerma - Ocoyoacac',
-    telefonos: ['728 103 3738', '722 688 6577'],
+    name: "Lerma - Ocoyoacac",
+    telefonos: ["728 103 3738", "722 688 6577"],
   },
   {
-    name: 'Morelia Norte',
-    telefonos: ['443 316 6924', '443 316 6951'],
+    name: "León",
+    telefonos: ["477 707 9077", "477 429 5423"],
   },
   {
-    name: 'Morelia Sur',
-    telefonos: ['443 234 0110', '443 327 4542'],
+    name: "Mérida",
+    telefonos: ["999 931 0712", "999 931 0704"],
   },
   {
-    name: 'Monterrey',
-    telefonos: ['818 340 4230', '818 340 4578'],
+    name: "Metepec | San Mateo | Xonacatlán",
+    telefonos: ["722 199 5617", "722 199 3759"],
   },
   {
-    name: 'Aguascalientes',
-    telefonos: ['449 994 1503', '449 994 2007'],
+    name: "Metepec sucursal Sur | Mexicalcingo | Calimaya | Ixtapan de la sal | Tenancingo",
+    telefonos: ["722 320 2005", "722 297 2284"],
   },
   {
-    name: 'Metepec | San Mateo | Xonacatlán',
-    telefonos: ['722 199 5617', '722 199 3759'],
+    name: "Monterrey",
+    telefonos: ["818 340 4230", "818 340 4578"],
   },
   {
-    name: 'Metepec sucursal Sur | Mexicalcingo | Calimaya | Ixtapan de la sal | Tenancingo',
-    telefonos: ['722 320 2005', '722 297 2284'],
+    name: "Morelia Norte",
+    telefonos: ["443 316 6924", "443 316 6951"],
   },
   {
-    name: 'Cancún',
-    telefonos: ['998 138 8210', '998 138 8212']
+    name: "Morelia Sur",
+    telefonos: ["443 234 0110", "443 327 4542"],
   },
   {
-    name: 'León',
-    telefonos: ['477 707 9077', '477 429 5423']
+    name: "San Luis Potosí",
+    telefonos: ["444 688 8097", "444 688 8098"],
   },
   {
-    name: 'Mérida',
-    telefonos: ['999 931 0712', '999 931 0704']
+    name: "Toluca | Atlacomulco | Zinacantepec",
+    telefonos: ["722 270 2114", "722 270 2115"],
   },
   {
-    name: 'Acapulco',
-    telefonos: ['744 482 1780', '744 482 1852']
+    name: "Toluca sucursal Colón | Valle de Bravo | Cacalomacán | Capultitán",
+    telefonos: ["722 277 8960", "722 2171276"],
   },
   {
-    name: 'Veracruz',
-    telefonos: ['229 935 5441', '229 935 6313']
+    name: "Veracruz",
+    telefonos: ["229 935 5441", "229 935 6313"],
   },
-]
+];
 
 const Sedes = () => {
-  const [textFilter, setTextFilter] = useState('')
+  const [textFilter, setTextFilter] = useState("");
 
   const filter = useCallback(
     (sede: sede) => sede.name.toLowerCase().includes(textFilter.toLowerCase()),
-    [textFilter],
-  )
+    [textFilter]
+  );
 
   return (
     <div className="md:grid md:grid-cols-2 gap-10 static min-h-screen">
@@ -105,7 +105,7 @@ const Sedes = () => {
           </h1>
           <p className="text-center text-xl">Nuestro trabajo nos respalda</p>
           <TextInput
-            icon={() => <MagnifyingGlassIcon className='text-slate-500 h-5'/>}
+            icon={() => <MagnifyingGlassIcon className="text-slate-500 h-5" />}
             placeholder="Busca tu sucursal"
             onChange={(data) => setTextFilter(data.target.value)}
           />
@@ -120,12 +120,12 @@ const Sedes = () => {
         <img
           src="/images/help.png"
           alt="Phone man"
-          style={{ height: '80vh' }}
+          style={{ height: "80vh" }}
           className="self-center md:fixed  md:my-16 object-contain"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sedes
+export default Sedes;
