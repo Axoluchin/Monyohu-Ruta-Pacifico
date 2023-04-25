008b064923ea6d5f7dc2f2571ba72a92d702a9b1
Damián Hernández Huchín <60200578+Axoluchin@users.noreply.github.com>
2023-04-24 18:50:07 -0600

    Update text in landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -22,7 +22,7 @@ const Landing = () => (
           <span className="font-bold">10% de descuento</span> en apoyo a tu
           economía
         </li>
-        <li>Revision gratis</li>
+        <li>Revision <span className="font-bold">GRATIS</span></li>
         <li>
           Contamos con <span className="font-bold">tecnicos capacitados</span>
         </li>
